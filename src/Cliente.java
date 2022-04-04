4c754013fde9875678430ba61a42f8d1b2b12f3a
HenriBDev <henribdev@gmail.com>
2022-04-03 21:31:03 -0300

    :sparkles: feat - Adicionado menu e CPF
    - Adicionado campo de CPF para o cliente
    - Adicionado menu interativo

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,9 +2,14 @@
 public class Cliente {
 
 	private String nome;
+	private int cpf;
 
 	public String getNome() {
 		return nome;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
 	}
 
 	public void setNome(String nome) {
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,52 @@
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Scanner scan = new Scanner(System.in);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
+		System.out.print("Olá, digite seus dados por favor: \n\nNome: ");
+		Cliente cliente = new Cliente();
+		Conta conta;
+		cliente.setNome(scan.nextLine());
+
+		System.out.print("Cpf: ");
+		cliente.setCpf(Integer.parseInt(scan.nextLine()));
+
+		System.out.print("Tipo de conta (1 - Corrente, 2 - Poupança): ");
+		int tipoConta = scan.nextInt();
 		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
+		if(tipoConta == 1){
+			conta = new ContaCorrente(cliente);
+		}else{
+			conta = new ContaPoupanca(cliente);
+		}
+		conta.depositar(100);
+
+		int selecao;
+		do{
+			System.out.print("\nBem-vindo " + cliente.getNome() + "! O que deseja fazer?\n\n1 - Depositar dinheiro\n2 - Sacar dinheiro\n3 - Imprimir extrato\n4 - Sair\n\nEscolha: ");
+			selecao = scan.nextInt();
+			switch(selecao){
+				case 1:
+					System.out.print("Digite o valor que deseja depositar: ");
+					conta.depositar(scan.nextDouble());
+				break;
+				case 2:
+					System.out.print("Digite o valor que deseja sacar: ");
+					conta.sacar(scan.nextDouble());
+				break;
+				case 3:
+					conta.imprimirExtrato();
+				break;
+				case 4:
+					System.out.print("Obrigado por ter usado o programa! ^^\n");
+				break;
+				default:
+					System.out.print("Seleção inválida!");
+				break;
+			}
+		}while(selecao != 4);
 	}
-
 }
